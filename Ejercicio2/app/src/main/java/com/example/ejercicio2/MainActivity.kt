e581539a60f4037edf4d77d1d2a6802fd0752b69
Jorge Arenas <jorgearenass97@gmail.com>
2021-10-05 13:14:16 +0200

    Funcionalidad implementada

--- Ejercicio2/app/src/main/java/com/example/ejercicio2/MainActivity.kt
+++ Ejercicio2/app/src/main/java/com/example/ejercicio2/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.ejercicio2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var boton:Button=findViewById(R.id.btnPulsame)
+        var cajaNum1:EditText=findViewById(R.id.edNum1)
+        var cajaNum2:EditText=findViewById(R.id.edNum2)
+        var resul:TextView=findViewById(R.id.txtResultado)
+
+        boton.setOnClickListener(){
+            var n1=Integer.parseInt(cajaNum1.text.toString())
+            var n2=Integer.parseInt(cajaNum2.text.toString())
+            var sum=n1+n2
+            resul.text="Resultado: $sum"
+            Toast.makeText(this,"Sumando...",Toast.LENGTH_SHORT).show()
+        }
     }
 }
