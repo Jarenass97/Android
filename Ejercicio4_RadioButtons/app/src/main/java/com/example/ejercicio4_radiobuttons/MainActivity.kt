31569f4e58647102fc64f080f1f3da86df77f679
Jorge Arenas <jorgearenass97@gmail.com>
2021-10-05 20:28:31 +0200

    Ej4_Radio: Terminado

--- Ejercicio4_RadioButtons/app/src/main/java/com/example/ejercicio4_radiobuttons/MainActivity.kt
+++ Ejercicio4_RadioButtons/app/src/main/java/com/example/ejercicio4_radiobuttons/MainActivity.kt
@@ -3,28 +3,22 @@ package com.example.ejercicio4_radiobuttons
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var botonSumar: Button = findViewById(R.id.btnSuma)
-        var botonRestar: Button = findViewById(R.id.btnResta)
-        var botonMultiplicar: Button = findViewById(R.id.btnMultiplica)
-        var botonDividir: Button = findViewById(R.id.btnDivide)
+        var botonSumar: Button = findViewById(R.id.rbSuma)
+        var botonRestar: Button = findViewById(R.id.rbResta)
+        var botonMultiplicar: Button = findViewById(R.id.rbMultiplica)
+        var botonDividir: Button = findViewById(R.id.rbDivide)
 
         var cajaNum1: EditText = findViewById(R.id.edNum1)
         var cajaNum2: EditText = findViewById(R.id.edNum2)
         var resul: TextView = findViewById(R.id.txtResultado)
-
-
         var res = 0
-
         botonSumar.setOnClickListener() {
             var n1 = Integer.parseInt(cajaNum1.text.toString())
             var n2 = Integer.parseInt(cajaNum2.text.toString())
@@ -55,22 +49,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    /**
-     *  0. Suma
-     *  1. Resta
-     *  2. Multiplica
-     *  3. Divide
-     */
-    private fun operacion(n1: Int, n2: Int, operacion: Int): Int {
-        var res = 0
-        when (operacion) {
-            0 -> res = n1 + n2
-            1 -> res = n1 - n2
-            2 -> res = n1 * n2
-            3 -> if (n2 != 0) res = n1 / n2
-        }
-        return res
+}
+
+/**
+ *  0. Suma
+ *  1. Resta
+ *  2. Multiplica
+ *  3. Divide
+ */
+private fun operacion(n1: Int, n2: Int, operacion: Int): Int {
+    var res = 0
+    when (operacion) {
+        0 -> res = n1 + n2
+        1 -> res = n1 - n2
+        2 -> res = n1 * n2
+        3 -> if (n2 != 0) res = n1 / n2
     }
-
-
+    return res
 }
